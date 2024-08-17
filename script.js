f199338a881598b04217584c504a2c9d6b7edce3
yellowcatx <isabelibbarques@gmail.com>
2024-08-17 12:24:57 -0300

    update �

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactLink").addEventListener("click", function(event) {
-    event.preventDefault(); // Previne o comportamento padrão do link
-    document.getElementById("footer").scrollIntoView({ behavior: "smooth" });
-  });
-  
